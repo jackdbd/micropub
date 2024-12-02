774dec90c15bba2c3b33ed0fb2ecede3858144ce
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-12-02 10:01:57 +0100

    allow empty string in p-content schema

--- src/lib/microformats2/p-content.ts
+++ src/lib/microformats2/p-content.ts
@@ -4,7 +4,8 @@ export const p_content = Type.String({
   $id: 'p-content',
   description: 'Some plain text content',
   title: 'content',
-  minLength: 1
+  // p-content can be an empty string (e.g. in bookmarks, likes, reposts)
+  minLength: 0
 })
 
 export type P_Content = Static<typeof p_content>
